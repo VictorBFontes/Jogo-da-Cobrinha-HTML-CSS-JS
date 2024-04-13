79a1128b8d6940ff7d3368700f4b87adbe4953f5
VictorBFontes <victor.jose.barros.fontes@gmail.com>
2024-04-13 00:34:04 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+window.addEventListenr("DOMContentLoaded", function (event) {
+    window.focus();
+
+    let snakePositions;
+    let applePosition;
+
+    let startTimestamp;
+    let lastTimestamp;
+    let stepsTaken;
+    let score;
+    let contrast;
+
+    let inputs; //Lista de direção vai fazer
+
+})
