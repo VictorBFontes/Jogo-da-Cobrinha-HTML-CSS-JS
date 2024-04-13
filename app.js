8d0ff25f847ff284bfb8632bb2ef72851eae26af
VictorBFontes <victor.jose.barros.fontes@gmail.com>
2024-04-13 01:36:26 -0300

    JavaScript function ResetGame 40%

--- app.js
+++ app.js
@@ -14,4 +14,55 @@ window.addEventListenr("DOMContentLoaded", function (event) {
 
     let inputs; //Lista de direção vai fazer
 
+    let gameStarted = false;
+    let hardMode = false;
+
+    const width = 15;
+    const height = 15;
+
+    const speed = 200;
+    let fadeSpeed = 5000;
+    let fadeExponential =1.024;
+
+    const contrastIncrease = 0.5;
+    const color = "black";
+
+
+    const grid = document.querySelector(".grid");
+    for (let i = 0; i < width * height; i++) {
+        const content = document.createElement("div");
+        content.setAttribute("class", "content");
+        content.setAttribute("id", i);
+
+        const tile = document.createElement("div");
+        tile.setAttribute("class", "tile");
+        tile.appendChild(content);
+
+        grid.appendChild(tile);
+    }
+
+    const tiles = document.querySelectorAll(".grid .tile .content");
+
+    const containerElement = document.querySelector(".container");
+    const noteElement = document.querySelector("footer");
+    const contrastElement = document.querySelector(".contrast");
+    const scoreElement = document.querySelector(".score");
+
+    resetGame();
+
+    function resetGame() {
+        snakePositions = [168, 169, 170, 171];
+        applePosition = 100;
+
+        startTimestamp = undefined;
+        lastTimestamp = undefined;
+        stepsTaken = -1;
+        score = 0;
+        contrast = 1;
+
+        inputs = [];
+
+        contrastElement.innerText = `${Math.floor(contrast * 100)}%`;
+        scoreElement.innerText = hardMode ? `H ${score}` : score;
+    }
 })
